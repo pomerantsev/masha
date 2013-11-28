2049b32dd728c9418d7b55023d6ede945b15d61b
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-11-28 17:06:17 +0400

    Revert initialize_on_precompile to true

--- config/application.rb
+++ config/application.rb
@@ -60,6 +60,6 @@ module Rickrockstar
     config.assets.version = '1.0'
 
     # Do not initialize the environment during asset precompilation.
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
   end
 end
