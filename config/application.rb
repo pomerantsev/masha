492eb6bf00671a054a7b6089a2394001e505029e
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-11-28 17:00:52 +0400

    Fix asset precompilation on heroku

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Rickrockstar
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Do not initialize the environment during asset precompilation.
+    config.assets.initialize_on_precompile = false
   end
 end
